43443e2bd288d82e86863c2791e84a9d29c1fb52
oskarrough <oskar@rough.dk>
2025-11-30 13:21:06 +0100

    Add latest_track_at field to channels view

--- supabase/migrations/20250905114538_channels-view.sql
+++ supabase/migrations/20250905114538_channels-view.sql
@@ -1,14 +1,16 @@
--- this adds a `track_count` field 
+-- this adds a `track_count` field and `latest_track_at`
 create or replace view channels_with_tracks
-	with (security_invoker=on)
-	as
-
-SELECT 
+with (security_invoker = on) as
+SELECT
   c.*,
-  COALESCE(tc.track_count, 0) AS track_count
+  COALESCE(tc.track_count, 0) AS track_count,
+  tc.latest_track_at
 FROM channels c
 LEFT JOIN (
-  SELECT channel_id, count(*) AS track_count 
-  FROM channel_track 
+  SELECT
+    channel_id,
+    count(*) AS track_count,
+    MAX(created_at) AS latest_track_at
+  FROM channel_track
   GROUP BY channel_id
-) tc ON c.id = tc.channel_id
+) tc ON c.id = tc.channel_id;
